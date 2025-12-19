e981b15351fdf13c79080a25c97390d4c3681d24
Diego Gonzalez <142106783+xDiegoAGG@users.noreply.github.com>
2025-12-19 01:25:11 -0500

    add pre-configuration from fabricmc generator

---
+++ src/client/java/com/diegoagg/gordasmod/GordasModClient.java
@@ -0,0 +1,10 @@
+package com.diegoagg.gordasmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GordasModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
